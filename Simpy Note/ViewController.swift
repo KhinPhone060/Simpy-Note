6d251867db29e6cb1584ea8a54d583c5d6f2c898
KhinPhone060 <60284009+KhinPhone060@users.noreply.github.com>
2023-03-12 13:58:54 +0630

    Initial Commit

---
+++ Simpy Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simpy Note
+//
+//  Created by Khin Phone Ei on 12/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
